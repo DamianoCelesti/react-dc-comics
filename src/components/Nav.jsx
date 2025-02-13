d61d5ede690f7803a0774538a92a536d9da0befd
DamianoCelesti <damiano.celesti@gmail.com>
2025-02-13 14:43:42 +0100

    added nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+const navItems = ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
+
+
+export default function Navbar() {
+    return (
+        <ul className="nav">
+            {navItems.map((navItem) => (
+                <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">{navItem}</a></li>
+            ))}
+
+        </ul>
+    );
+}
+
+
+
